467db33b1e5b9cb4798c99e6e04d9fe6a3fc4b0f
Vasily93 <markovavasily@gmail.com>
2020-03-05 18:48:25 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello vasily WHAT'S UP!
+          hello vasily WHAT'S UPpppp!
         </p>
         <a
           className="App-link"
